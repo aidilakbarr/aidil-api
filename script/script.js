c0e7884adee7bc7ba1c414ce2f4be8a53498dacd
Aidilakbarr <akbaraidil464@gmail.com>
2023-08-03 21:09:30 +0800

    refactoring

--- script/script.js
+++ script/script.js
@@ -1,206 +1,169 @@
-// function getDataMahasiswa(url, success, error) {
-//   const xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status === 200) {
-//         success(xhr.responseText);
-//       }
-//     } else {
-//       error(xhr.response);
-//     }
-//   };
+// // ajax http ke fetch api
 
-//   xhr.open("get", url);
-//   xhr.send();
-// }
-
-// const boxDetail = document.querySelector(".modal-box");
-
-// function success(result) {
-//   hasil = JSON.parse(result);
-//   movies = hasil.Search;
-//   let card = "";
-//   movies.forEach((movies) => {
-//     card += `
-//         <div class="card">
-//         <img src="${movies.Poster}" class="poster" />
-//         <div class="judul-film">${movies.Title}</div>
-//         <div class="tahun">tahun terbit :${movies.Year}</div>
-//         <button class="detail" data-imdbid="${movies.imdbID}">rincian</button>
-//       </div>`;
-//   });
-
-//   let cards = document.querySelector(".cards");
-//   cards.innerHTML = card;
-
-//   const detailElements = document.querySelectorAll(".detail");
-
-//   detailElements.forEach(function (detail) {
-//     detail.addEventListener("click", function (e) {
-//       let imdbid = detail.getAttribute("data-imdbid");
-//       console.log(imdbid);
-
-//       // menjalankan api baru
-//       function getDetailMovies(url, success, error) {
-//         const xhr = new XMLHttpRequest();
-//         xhr.onreadystatechange = function () {
-//           if (xhr.readyState === 4) {
-//             if (xhr.status === 200) {
-//               success(xhr.responseText);
-//             }
-//           } else {
-//             error(xhr.response);
-//           }
-//         };
-
-//         xhr.open("get", url);
-//         xhr.send();
-//       }
-
-//       // fungsi suksess
-//       function sukses(result) {
-//         hasil = JSON.parse(result);
-//         modalbox = `
-//         <img
-//         class="poster"
-//         src="${hasil.Poster}"
-//       />
-//       <div class="desc">
-//         <div class="judulfilm"><strong>judul film : </strong>${hasil.Title}</div>
-//         <div class="tahun-rilis"><strong>tahun rilis : </strong>${hasil.Year}</div>
-//         <div class="durasi"><strong>durasi : </strong>${hasil.Runtime}</div>
-//         <div class="genre">
-//           <strong>genre : </strong>${hasil.Genre}
-//         </div>
-//         <div class="sinopsis">
-//           <strong>plot : </strong>${hasil.Plot}
-//         </div>
-//         <button class="close">close</button>
-//         </div>
-//         `;
-
-//         boxDetail.innerHTML = modalbox;
-
-//         const overlay = document.querySelector(".overlay");
-//         overlay.style.display = "block";
-
-//         if (boxDetail.classList.contains("active")) {
-//           boxDetail.classList.remove("active");
-//         }
-
-//         const tombol = document.querySelector(".close");
-//         tombol.addEventListener("click", function () {
-//           overlay.style.display = "none";
-//           if (!boxDetail.classList.contains("active")) {
-//             boxDetail.classList.add("active");
-//           }
-//         });
-//       }
-
-//       function eror(result) {
-//         console.log("ERROR!");
-//       }
-
-//       // menjalankan fungsi api ke dua
-//       getDetailMovies(
-//         `https://www.omdbapi.com/?apikey=441188f8&i=${imdbid}`,
-//         sukses,
-//         eror
-//       );
-//     });
-//   });
-// }
-
-// function error(result) {
-//   console.log("ERROR");
-// }
-
-// const input = document.getElementById("search");
 // const tombolInput = document.querySelector(".tombol");
 // tombolInput.addEventListener("click", function () {
+//   const input = document.getElementById("search");
 //   nilaiInput = input.value;
 
 //   // menjalankan API pertama
-//   getDataMahasiswa(
-//     `https://www.omdbapi.com/?apikey=441188f8&s=${nilaiInput}`,
-//     success,
-//     error
-//   );
+//   fetch(`https://www.omdbapi.com/?apikey=441188f8&s=${nilaiInput}`)
+//     .then((response) => response.json())
+//     .then((response) => {
+//       const movies = response.Search;
+//       let cards = "";
+//       movies.forEach((movie) => {
+//         cards += `
+//         <div class="card">
+//           <img src="${movie.Poster}" class="poster" />
+//           <div class="judul-film">${movie.Title}</div>
+//           <div class="tahun">tahun terbit :${movie.Year}</div>
+//           <button class="detail" data-imdbid="${movie.imdbID}">rincian</button>
+//         </div>`;
+//       });
+//       let card = document.querySelector(".cards");
+//       card.innerHTML = cards;
+
+//       const detailElements = document.querySelectorAll(".detail");
+
+//       detailElements.forEach(function (detail) {
+//         detail.addEventListener("click", function (e) {
+//           let imdbid = detail.getAttribute("data-imdbid");
+
+//           // menjalankan api kedua
+//           fetch(`https://www.omdbapi.com/?apikey=441188f8&i=${imdbid}`)
+//             .then((response) => response.json())
+//             .then((response) => {
+//               let modalbox = `<img
+//               class="poster"
+//               src="${response.Poster}"
+//             />
+//             <div class="desc">
+//               <div class="judulfilm"><strong>judul film : </strong>${response.Title}</div>
+//               <div class="tahun-rilis"><strong>tahun rilis : </strong>${response.Year}</div>
+//               <div class="durasi"><strong>durasi : </strong>${response.Runtime}</div>
+//               <div class="genre">
+//                 <strong>genre : </strong>${response.Genre}
+//               </div>
+//               <div class="sinopsis">
+//                 <strong>plot : </strong>${response.Plot}
+//               </div>
+//               <button class="close">close</button>
+//               </div>`;
+
+//               const boxDetail = document.querySelector(".modal-box");
+//               boxDetail.innerHTML = modalbox;
+
+//               const overlay = document.querySelector(".overlay");
+//               overlay.style.display = "block";
+
+//               if (boxDetail.classList.contains("active")) {
+//                 boxDetail.classList.remove("active");
+//               }
+
+//               const tombol = document.querySelector(".close");
+//               tombol.addEventListener("click", function () {
+//                 overlay.style.display = "none";
+//                 if (!boxDetail.classList.contains("active")) {
+//                   boxDetail.classList.add("active");
+//                 }
+//               });
+//             })
+//             .catch((response) => console.log(response));
+//         });
+//       });
+//     })
+//     .catch((response) => console.log(response));
 // });
 
 // ajax http ke fetch api
 
-const input = document.getElementById("search");
 const tombolInput = document.querySelector(".tombol");
-tombolInput.addEventListener("click", function () {
-  nilaiInput = input.value;
-
-  // menjalankan API pertama
-  fetch(`https://www.omdbapi.com/?apikey=441188f8&s=${nilaiInput}`)
-    .then((response) => response.json())
-    .then((response) => {
-      const movies = response.Search;
-      let cards = "";
-      movies.forEach((movie) => {
-        cards += `
-        <div class="card">
-          <img src="${movie.Poster}" class="poster" />
-          <div class="judul-film">${movie.Title}</div>
-          <div class="tahun">tahun terbit :${movie.Year}</div>
-          <button class="detail" data-imdbid="${movie.imdbID}">rincian</button>
-        </div>`;
-      });
-      let card = document.querySelector(".cards");
-      card.innerHTML = cards;
-
-      const detailElements = document.querySelectorAll(".detail");
-
-      detailElements.forEach(function (detail) {
-        detail.addEventListener("click", function (e) {
-          let imdbid = detail.getAttribute("data-imdbid");
-
-          // menjalankan api kedua
-          fetch(`https://www.omdbapi.com/?apikey=441188f8&i=${imdbid}`)
-            .then((response) => response.json())
-            .then((response) => {
-              let modalbox = `<img
-              class="poster"
-              src="${response.Poster}"
-            />
-            <div class="desc">
-              <div class="judulfilm"><strong>judul film : </strong>${response.Title}</div>
-              <div class="tahun-rilis"><strong>tahun rilis : </strong>${response.Year}</div>
-              <div class="durasi"><strong>durasi : </strong>${response.Runtime}</div>
-              <div class="genre">
-                <strong>genre : </strong>${response.Genre}
-              </div>
-              <div class="sinopsis">
-                <strong>plot : </strong>${response.Plot}
-              </div>
-              <button class="close">close</button>
-              </div>`;
-
-              const boxDetail = document.querySelector(".modal-box");
-              boxDetail.innerHTML = modalbox;
-
-              const overlay = document.querySelector(".overlay");
-              overlay.style.display = "block";
-
-              if (boxDetail.classList.contains("active")) {
-                boxDetail.classList.remove("active");
-              }
-
-              const tombol = document.querySelector(".close");
-              tombol.addEventListener("click", function () {
-                overlay.style.display = "none";
-                if (!boxDetail.classList.contains("active")) {
-                  boxDetail.classList.add("active");
-                }
-              });
-            })
-            .catch((response) => console.log(response));
-        });
-      });
-    })
-    .catch((response) => console.log(response));
+tombolInput.addEventListener("click", async function () {
+  const input = document.getElementById("search");
+  const film = await getMovies(input.value);
+  updateUI(film); // Memanggil fungsi updateUI untuk memperbarui tampilan
 });
+
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("detail")) {
+    const imdbid = e.target.dataset.imdbid;
+    const movieDetail = await getMovieDetail(imdbid);
+    const modalbox = document.querySelector(".modal-box");
+    if (modalbox.classList.contains("active")) {
+      modalbox.classList.remove("active");
+    }
+    updateUIDetail(movieDetail);
+    menambahkanOverlay();
+    const tombolClose = document.querySelector(".close");
+    tombolClose.addEventListener("click", function () {
+      if (!modalbox.classList.contains("active")) {
+        modalbox.classList.add("active");
+        menutupOverlay();
+      }
+    });
+  }
+});
+
+const overlay = document.querySelector(".overlay");
+
+function menambahkanOverlay() {
+  overlay.style.display = "block";
+}
+
+function menutupOverlay() {
+  overlay.style.display = "none";
+}
+
+function getMovieDetail(imdbid) {
+  return fetch(`https://www.omdbapi.com/?apikey=441188f8&i=${imdbid}`)
+    .then((response) => response.json())
+    .then((m) => m);
+}
+
+function updateUIDetail(m) {
+  let modalbox = menambahkanModalBox(m);
+  const boxDetail = document.querySelector(".modal-box");
+  boxDetail.innerHTML = modalbox;
+}
+
+function getMovies(keyword) {
+  return fetch(`https://www.omdbapi.com/?apikey=441188f8&s=${keyword}`)
+    .then((response) => response.json())
+    .then((response) => response.Search);
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((movie) => (cards += menambahkanCard(movie)));
+  const card = document.querySelector(".cards");
+  card.innerHTML = cards;
+}
+
+function menambahkanCard(movie) {
+  return `
+  <div class="card">
+    <img src="${movie.Poster}" class="poster" />
+    <div class="judul-film">${movie.Title}</div>
+    <div class="tahun">tahun terbit: ${movie.Year}</div>
+    <button class="detail" data-imdbid="${movie.imdbID}">rincian</button>
+  </div>`;
+}
+
+function menambahkanModalBox(m) {
+  return `<img
+  class="poster"
+  src="${m.Poster}"
+/>
+<div class="desc">
+  <div class="judulfilm"><strong>judul film : </strong>${m.Title}</div>
+  <div class="tahun-rilis"><strong>tahun rilis : </strong>${m.Year}</div>
+  <div class="durasi"><strong>durasi : </strong>${m.Runtime}</div>
+  <div class="genre">
+    <strong>genre : </strong>${m.Genre}
+  </div>
+  <div class="sinopsis">
+    <strong>plot : </strong>${m.Plot}
+  </div>
+  <button class="close">close</button>
+  </div>`;
+}
